dd25377664c3a37f2e5878f43080ce1bb094788b
Alan Antonuk <alan.antonuk@gmail.com>
2014-08-13 21:34:11 -0700

    Update rabbitmq-c formula to v0.5.1
    Update rabbitmq-c:
    - Latest version of package is v0.5.1
    - Codegen is no longer needed

--- Library/Formula/rabbitmq-c.rb
+++ Library/Formula/rabbitmq-c.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class RabbitmqC < Formula
   homepage "https://github.com/alanxz/rabbitmq-c"
-  url "https://github.com/alanxz/rabbitmq-c/archive/v0.5.0.tar.gz"
-  sha1 "826286c3f04695bdc231d8e7b0541f871975cdcc"
+  url "https://github.com/alanxz/rabbitmq-c/archive/v0.5.1.tar.gz"
+  sha1 "3a2fad69f65ef3a733fbfd9320717d2aedec5aa2"
 
   head "https://github.com/alanxz/rabbitmq-c.git"
 
@@ -13,17 +13,9 @@ class RabbitmqC < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "rabbitmq"
-  depends_on "simplejson" => :python if MacOS.version <= :leopard
-
-  resource "codegen" do
-    url "https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_3_1.tar.gz"
-    sha1 "62cfff4d3d3707b263a4e1b0d5d3b094e39d24f1"
-  end
 
   def install
     ENV.universal_binary if build.universal?
-    (buildpath/"codegen").install resource("codegen")
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
